d51aeb0da5adefe696f1b7b5987174c87c2b223f
Dang Nguyen <dangnguyen2207@gmail.com>
2019-06-14 21:26:22 -0500

    changes1

---
+++ wdn/templates_4.1/scripts/main-wdn-plugins.js
@@ -0,0 +1,17 @@
+define([
+	'analytics',
+	'navigation',
+	'search',
+	'socialmediashare',
+	'unlalert',
+	'images',
+	'form_validation',
+	'wdn-ui'
+], function() {
+	for (var i = 0, pluginCount = arguments.length; i < pluginCount; i++) {
+		var pluginObj = arguments[i];
+		if (pluginObj && "initialize" in pluginObj) {
+			pluginObj.initialize();
+		}
+	}
+});
